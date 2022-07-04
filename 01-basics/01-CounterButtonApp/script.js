6f2ce4215634d7db969a1ca9a9c7efba86740767
Nayil Fatykhov <nfatykhov82@gmail.com>
2022-07-04 18:20:58 +0300

    CounterButtonApp implementation

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,29 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+import {defineComponent} from "./vendor/vue.esm-browser";
 
 // Создайте Vue приложение
+
+const App = defineComponent({
+
+  data() {
+    let counter = 0;
+    return {
+      foo: 'bar',
+      counter
+    }
+  },
+
+  methods: {
+    increment(event) {
+      this.counter++;
+    }
+  }
+});
+
+
+// Создаём новое приложение по корневому компоненту
+const app = createApp(App);
+
+// Монтируем приложение на странице
+// Возвращается публичный экземпляр корневого компонента
+const vm = app.mount('#app');
