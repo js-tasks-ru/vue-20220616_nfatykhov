299ed26c185820979f9dd26b6009c1994909d137
Nayil Fatykhov <nfatykhov82@gmail.com>
2022-07-12 21:07:54 +0300

    03-MeetupCover implementation

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -3,8 +3,26 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupCover',
 
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    image: {
+      required: false,
+      default: "",
+    },
+  },
+
+  computed: {
+    bgUrl() {
+      return this.image ? `--bg-url: url('${this.image}')` : null;
+    },
+  },
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="bgUrl">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
